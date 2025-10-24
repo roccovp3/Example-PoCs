53c5218b12e422d46b1e3c3f342c1f6af4423da3
Mircea Tatulescu <mirceatatulescu@utexas.edu>
2025-10-24 00:14:37 -0500

    IDK what changed lol

--- MAC_Spectre/Evict_Simple/eviction_set_main.c
+++ MAC_Spectre/Evict_Simple/eviction_set_main.c
@@ -8,17 +8,17 @@
 char *main(int argc, char *argv[]) {
     assert(argc > 1 && "I need the malicious address");
 
-    uint64_t malicious_index = strtoul(argv[1], NULL, 16);
+    uint64_t secret_address = strtoul(argv[1], NULL, 16);
 
     // L1 Eviction set
     cache_line_set_t *L1_evict =
-        find_L1_congruent_cache_lines((uint8_t *)malicious_index, PAGE_SIZE);
+        find_L1_congruent_cache_lines((uint8_t *)secret_address, PAGE_SIZE);
 
     // L2 Eviction set
     cache_line_set_t *l2_congruent_cache_lines_1 =
-        find_L2_eviction_set_using_timer((uint8_t *)malicious_index);
+        find_L2_eviction_set_using_timer((uint8_t *)secret_address);
     cache_line_set_t *l2_congruent_cache_lines_2 =
-        find_L2_eviction_set_using_timer((uint8_t *)malicious_index);
+        find_L2_eviction_set_using_timer((uint8_t *)secret_address);
 
     cache_line_set_t *l2_congruent_cache_lines = merge_two_cache_line_sets(
         l2_congruent_cache_lines_1, l2_congruent_cache_lines_2);
