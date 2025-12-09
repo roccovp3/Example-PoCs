418de8b43192df6c8ffea3378cc28fc7378a2411
Mircea Tatulescu <mirceatatulescu@utexas.edu>
2025-12-08 21:09:22 -0600

    Please:

---
+++ BranchDifferent/common/cache.h
@@ -0,0 +1,16 @@
+#ifndef CACHE_H
+#define CACHE_H
+
+#include "config.h"
+
+
+
+struct cache_ctx;
+typedef struct cache_ctx* cache_ctx_t;
+
+
+cache_ctx_t cache_remove_prepare(char* address);
+
+void cache_remove(cache_ctx_t ctx);
+
+#endif /* CACHE_H */
---
+++ BranchDifferent/common/timing.h
@@ -0,0 +1,29 @@
+#ifndef TIMING_H
+#define TIMING_H
+
+#include <stdint.h>
+
+#include "config.h"
+#include "memory.h"
+
+
+    extern uint64_t timestamp;
+    #define timer_read(x) x = timestamp
+
+static inline __attribute__((always_inline)) uint64_t probe(char* address){
+    register uint64_t start, end;
+    memory_fence();
+    timer_read(start);
+    memory_fence();
+    memory_access(address);
+    memory_fence();
+    timer_read(end);
+    memory_fence();
+    return end - start;
+}
+
+void timer_start();
+
+void timer_stop();
+
+#endif /* TIMING_H */
